217d7ba7a7a3e797ffd3f542c6abb36b80eb51ab
ryokoko <llzstella@yahoo.com>
2024-09-29 16:46:19 +1000

    added message

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ import "./style.css";
 
 for (let i = 1; i <= 9; i++) {
   console.log("i = " + i);
-  var result_arr = [];
+  var result_arr = [];   //create an array for each loop
   for (let j = 1; j <= i; j++) {
     var result = j + " * " + i + " = " + j * i;
-    result_arr.push(result);
+    result_arr.push(result);  //append results to the current array
     // console.log(result_arr.join(','));
     // console.log(result_arr);
   }
-  console.log(result_arr.join(", "));
+  console.log(result_arr.join(", ")); //print result array by concatenating its elements
 }
+
+
 
 // for (let i = 1; i <= 9; i++) {
 //   console.log("i = " + i);
